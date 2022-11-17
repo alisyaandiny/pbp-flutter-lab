3aa0e7e7c1b01ee987e8e0abbfcd853866e66586
Alisya Andiny Alhabsyi <113089408+alisyaandiny@users.noreply.github.com>
2022-11-17 11:55:18 +0700

    Tugas 8 Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_7/drawer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,18 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tugas 7 PBP',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Program Counter'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
+
+  final String title = 'Flutter Demo Home Page';
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -30,97 +41,80 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String result = 'GENAP';
-  bool isShow = false;
 
   void _incrementCounter() {
     setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
-      isShow = true;
     });
   }
 
-  void _decrementCounter() {
+  void _turun() {
     setState(() {
-      if (_counter < 2) {
-        isShow = false;
-        _counter = 0;
-      } else {
-        _counter--;
-        isShow = true;
-      }
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      if (_counter > 0) _counter--;
     });
-  }
-
-  Text changeColor() {
-    if (_counter % 2 == 1) {
-      result = 'GANJIL';
-      return Text(
-        result,
-        style: const TextStyle(color: Colors.blue),
-      );
-    } else {
-      result = 'GENAP';
-      return Text(
-        result,
-        style: const TextStyle(color: Colors.red),
-      );
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-          padding: const EdgeInsets.all(10),
-          child: Stack(
-            children: [
-              Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                      height: 100,
-                      width: 300,
-                      child: Column(children: <Widget>[
-                        changeColor(),
-                        Text(
-                          '$_counter',
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                      ]))),
-              Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Container(
-                      height: 70,
-                      width: 70,
-                      child: Visibility(
-                        visible: isShow,
-                        child: FloatingActionButton(
-                          onPressed: () {
-                            _decrementCounter();
-                            changeColor();
-                          },
-                          tooltip: 'Decrement',
-                          child: const Icon(Icons.remove),
-                        ),
-                      ))),
-              Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                      height: 70,
-                      width: 70,
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          _incrementCounter();
-                          changeColor();
-                        },
-                        tooltip: 'Increment',
-                        child: const Icon(Icons.add),
-                      ))),
+        appBar: AppBar(
+          title: const Text("Counter Tugas PBP"),
+        ),
+        drawer: const myDrawer(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _counter % 2 == 0
+                  ? const Text(
+                      'GENAP',
+                      style: TextStyle(color: Colors.red),
+                    )
+                  : const Text(
+                      'GANJIL',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
             ],
-          )),
-    );
+          ),
+        ),
+        floatingActionButton: Visibility(
+            child: Container(
+                width: 361,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Visibility(
+                      visible: (_counter != 0),
+                      child: FloatingActionButton(
+                        heroTag: null,
+                        onPressed: _turun,
+                        tooltip: 'Decrement',
+                        child: const Icon(Icons.remove),
+                      ),
+                    ),
+                    FloatingActionButton(
+                      heroTag: null,
+                      onPressed: _incrementCounter,
+                      tooltip: 'Increment',
+                      child: const Icon(Icons.add),
+                    ),
+                  ],
+                )))
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
